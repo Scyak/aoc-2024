db05ca516a9217c3d4b24631a3085497784af4fa
Jay Sommer <jay.sommer@protonmail.com>
2024-12-01 08:36:52 +0000

    Solve day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -5,11 +5,51 @@ use crate::days::Day;
 pub struct Day01;
 
 impl Day for Day01 {
-    fn part_one(_input: &str) -> impl Display {
-        "no solution yet :("
+    fn part_one(input: &str) -> impl Display {
+        let mut list_one = vec![];
+        let mut list_two = vec![];
+
+        for line in input.lines() {
+            let locations: Vec<i64> = line
+                .split_whitespace()
+                .map(|num: &str| num.parse().unwrap())
+                .collect();
+            list_one.push(locations[0]);
+            list_two.push(locations[1]);
+        }
+
+        list_one.sort();
+        list_two.sort();
+
+        let mut total_difference = 0;
+
+        for (index, location) in list_one.iter().enumerate() {
+            total_difference += (location - list_two[index]).abs();
+        }
+
+        total_difference
     }
-    fn part_two(_input: &str) -> impl Display {
-        "no solution yet :("
+    fn part_two(input: &str) -> impl Display {
+        let mut list_one = vec![];
+        let mut list_two = vec![];
+
+        for line in input.lines() {
+            let locations: Vec<i64> = line
+                .split_whitespace()
+                .map(|num: &str| num.parse().unwrap())
+                .collect();
+            list_one.push(locations[0]);
+            list_two.push(locations[1]);
+        }
+
+        let mut similiarity = 0;
+
+        for location in list_one.iter() {
+            let occurences = list_two.iter().filter(|loc2| *loc2 == location).count();
+            similiarity += location * occurences as i64;
+        }
+
+        similiarity
     }
     fn get_day_num() -> u8 {
         return 1;
