98b3e49c2a39f64c41f915c0b48febd87e7ef490
Jay Sommer <jay.sommer@protonmail.com>
2024-12-03 09:42:37 +0000

    Solve day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fmt::Display;
 
 use crate::days::Day;
@@ -5,12 +6,40 @@ use crate::days::Day;
 pub struct Day03;
 
 impl Day for Day03 {
-    fn part_one(_input: &str) -> impl Display {
-        "no solution yet :("
+    fn part_one(input: &str) -> impl Display {
+        let re = Regex::new(r"mul\((\d*),(\d*)\)").unwrap();
+
+        let mut sum: i128 = 0;
+        for (_, [arg1, arg2]) in re.captures_iter(input).map(|c| c.extract()) {
+            sum += arg1.parse::<i128>().unwrap() * arg2.parse::<i128>().unwrap();
+        }
+
+        sum
     }
-    fn part_two(_input: &str) -> impl Display {
-        "no solution yet :("
+
+    fn part_two(input: &str) -> impl Display {
+        let re = Regex::new(r"(mul|don't|do)\((?:(\d*),(\d*))?\)").unwrap();
+
+        let mut sum: i128 = 0;
+        let mut multiplying = true;
+
+        for captures in re.captures_iter(input) {
+            match &captures[1] {
+                "mul" => {
+                    if multiplying {
+                        sum += captures[2].parse::<i128>().unwrap()
+                            * captures[3].parse::<i128>().unwrap();
+                    }
+                }
+                "do" => multiplying = true,
+                "don't" => multiplying = false,
+                _ => (),
+            }
+        }
+
+        sum
     }
+
     fn get_day_num() -> u8 {
         return 3;
     }
